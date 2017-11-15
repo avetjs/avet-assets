fae477172b51380d364b91a731ce2745631af736
okoala <dapixp@gmail.com>
2017-11-15 10:51:56 +0800

    feat: first commit

---
+++ config/config.default.js
@@ -0,0 +1,18 @@
+exports.assets = {
+  extensions: [ 'txt', 'svg', 'png' ],
+  regExp: '.*/static/(.+)',
+  name: '/static/[1]?[sha512:hash:base64:7]',
+};
+
+exports.build = {
+  babel: (babelConfig, config) => {
+    babelConfig.plugins.push([
+      require.resolve('babel-plugin-transform-assets'),
+      {
+        extensions: config.assets.extensions,
+        regExp: config.assets.regExp,
+        name: config.assets.name,
+      },
+    ]);
+  },
+};
