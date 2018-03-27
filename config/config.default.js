a20b5bc930de9c22c6d31ebf2864721a5e8d1261
okoala <dapixp@gmail.com>
2018-03-27 23:06:55 +0800

    feat: support assertPrefix

--- config/config.default.js
+++ config/config.default.js
@@ -1,17 +1,29 @@
 exports.assets = {
-  extensions: ['txt', 'svg', 'png', 'jpg', 'jpeg', 'webp', 'gif'],
+  extensions: [ 'txt', 'svg', 'png', 'jpg', 'jpeg', 'webp', 'gif' ],
   regExp: '.*/static/(.+)',
   name: '/static/[1]?[sha512:hash:base64:7]',
 };
 
 exports.build = {
   babel: (babelConfig, config) => {
+    let { name } = config.assets;
+
+    if (config.build.assetPrefix) {
+      const uuid = require('uuid');
+      const buildId = uuid.v4();
+      config.build.buildId = buildId;
+
+      name =
+        config.build.assetPrefix.replace(/\/$/, '') +
+        name.replace('static', `static/${buildId}`);
+    }
+
     babelConfig.plugins.push([
       require.resolve('babel-plugin-transform-assets'),
       {
         extensions: config.assets.extensions,
         regExp: config.assets.regExp,
-        name: config.assets.name,
+        name,
       },
     ]);
 
